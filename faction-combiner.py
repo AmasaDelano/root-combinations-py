8bb79cd59d4fedb946116dee3e09e4ecb1d45dac
Alex Leone <me@alexjleone.com>
2021-11-26 22:57:43 -0600

    Added column for Valid ADSET combinations

--- faction-combiner.py
+++ faction-combiner.py
@@ -24,11 +24,13 @@ reach_minimums = {
     6: 28
 }
 
+red_adset_factions = ["Marquise de Cat", "Lord of the Hundreds", "Keepers in Iron", "Underground Duchy", "Eyrie Dynasties"]
+
 min_player_count = 2
 max_player_count = 6
 
 # SET UP CSV FILE HEADER (FIRST ROW).
-file_output = "Num Players, Total Reach, Recommended, " + ", ".join(reach_values) + "\n"
+file_output = "Num Players, Total Reach, Recommended Reach, Valid ADSET, " + ", ".join(reach_values) + "\n"
 
 for num_players in range(min_player_count, max_player_count+1):
     player_combinations = list(itertools.combinations(reach_values, num_players))
@@ -40,16 +42,25 @@ for num_players in range(min_player_count, max_player_count+1):
 
         total_reach = sum(map(lambda faction: reach_values[faction], combination))
 
-        player_and_reach_info = str(num_players) + ", " + str(total_reach) + ", "
-        faction_list = (", ".join(map(lambda faction: "YES" if (faction in combination) else "-", reach_values))) + "\n"
+        file_output += str(num_players) + ", " + str(total_reach) + ", "
 
         # IS THIS A RECOMMENDED COMBINATION, AN "ADVENTUROUS" COMBINATION, OR NOT VIABLE?
         if total_reach >= reach_minimums[num_players]:
-            file_output += player_and_reach_info + "Yes, " + faction_list
+            file_output += "Yes, "
         elif total_reach >= 17:
-            file_output += player_and_reach_info + "Exp., " + faction_list
+            file_output += "Exp., "
         elif total_reach < 17: 
-            file_output += player_and_reach_info + "No, " + faction_list
+            file_output += "No, "
+
+        # IS THIS A VALID COMBINATION USING ADVANCED SETUP (ADSET)?
+        num_red_adset_factions = len(set(combination).intersection(red_adset_factions))
+        if (num_players == 2 and num_red_adset_factions == 2) or (num_players >= 3 and num_red_adset_factions >= 1):
+            file_output += "Yes, "
+        else:
+            file_output += "No, "
+
+        faction_list = (", ".join(map(lambda faction: "YES" if (faction in combination) else "-", reach_values)))
+        file_output += faction_list + "\n"
 
 # OVERWRITE CSV FILE.
 with open(os.path.join(sys.path[0], "combinations.csv"), "w+") as file:
